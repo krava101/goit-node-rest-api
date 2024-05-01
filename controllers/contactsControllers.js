41ca68e845f1d84788b566d30173d5df056a35b5
krava <kravalex2004@gmail.com>
2024-05-01 11:04:50 +0300

    Update contactsControllers.js

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,7 +4,7 @@ import * as contactsService from "../services/contactsServices.js";
 export const getAllContacts = async (_, res, next) => {
   try {
     const contacts = await contactsService.listContacts();
-    res.status(200).send({ results: contacts });
+    res.status(200).send(contacts);
   } catch (err){
     next(err);
   }
@@ -14,7 +14,7 @@ export const getOneContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const contact = await contactsService.getContactById(id);
-    contact ? res.status(200).send(contact) : res.status(404).send({ status: 404, message: "Not found" });
+    contact ? res.status(200).send(contact) : res.status(404).send({message: "Not found" });
   } catch (err) {
     next(err);
   }
@@ -24,7 +24,7 @@ export const deleteContact = async (req, res, next) => {
   try {
     const { id } = req.params;
     const contact = await contactsService.removeContact(id);
-    contact ? res.status(200).send(contact) : res.status(404).send({ status: 404, message: "Not found" });
+    contact ? res.status(200).send(contact) : res.status(404).send({message: "Not found" });
   } catch (err) {
     next(err);
   }
@@ -33,7 +33,7 @@ export const deleteContact = async (req, res, next) => {
 export const createContact = async (req, res, next) => {
   const contactValidate = createContactSchema.validate(req.body);
   if (contactValidate.error) {
-    return res.status(400).send({status: 400, message: contactValidate.error.details[0].message });
+    return res.status(400).send({message: contactValidate.error.details[0].message });
   } else {
     try {
       const contact = await contactsService.addContact(req.body);
@@ -51,11 +51,11 @@ export const updateContact = async (req, res, next) => {
   }
   const contactValidate = updateContactSchema.validate(req.body);
   if (contactValidate.error) {
-    return res.status(400).send({status: 400, message: contactValidate.error.details[0].message });
+    return res.status(400).send({message: contactValidate.error.details[0].message });
   } else {
     try {
       const contact = await contactsService.updateContact(id, req.body);
-      contact ? res.status(200).send(contact) : res.status(404).send({ status: 404, message: "Not found" });
+      contact ? res.status(200).send(contact) : res.status(404).send({message: "Not found" });
     } catch (err) {
       next(err);
     }
